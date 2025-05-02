a262e6d94cda7fc42e9ff25d26c80e6d8c2a17fb
ritspun <ritspun96@gmail.com>
2025-05-01 18:05:15 -0600

    Add new images for the menu icons like when closing and opening it. Responsive design updated, more adapted to new icons. li menu items hover color changed to a new one that is clearly better option.

--- Javascript/backToTop.js
+++ Javascript/backToTop.js
@@ -2,8 +2,8 @@
 var btn = document.getElementById("button");
 var btn2 = document.getElementById("button2");
 var checkMenu = document.getElementById('boton_Menu');
+var abrirElMenu = document.getElementById('menuAbrir');
 var cierreDelMenu = document.getElementById('menuCierre');
-var cierreDelMenu2 = document.getElementById('menuCierre2');
 
 function mostrarBoton() {
   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
@@ -35,7 +35,7 @@ function scrollToTop(evt) {
 document.addEventListener("click", (e) => {
   //Si el menuCheck y el cierreDelMenu no es el elemento clickado se cierra el menu, o se pasa a False el checkbox para que se cierre
   //porque estaria haciendo click en cualquier lado menos en el lugar en donde se abre o cierra el menu normalmente
-  if (!(checkMenu.contains(e.target)) && !(cierreDelMenu.contains(e.target)) && !(cierreDelMenu2.contains(e.target))) {
+  if (!(checkMenu.contains(e.target)) && !(abrirElMenu.contains(e.target)) && !(cierreDelMenu.contains(e.target))) {
     checkMenu.checked = false;
     //console.log("DOCUMENT LISTENER: FALSE");
   }
